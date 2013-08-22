89e2f722098ca540b099f99d403ca81b21350f56
catxxx <akfish@qq.com>
2013-08-22 21:19:04 +0800

    Update MathJax.js
    Add some

--- MathJax.js
+++ MathJax.js
@@ -1,3 +1,22 @@
 (function (BASENAME) {
+  var BASE = window[BASENAME];
+  if (!BASE) {
+    BASE = window[BASENAME] = {}
+  }
+
+  var PROTO = [];  // a static object used to indicate when a prototype is being created
+  var OBJECT = function (def) {
+    var obj = def.constructor; 
+    
+    if (!obj) {obj = new Function("")}
+    
+    for (var id in def) {
+      if (id !== 'constructor' && def.hasOwnProperty(id)) {
+        obj[id] = def[id]
+      }
+    }
+    
+    return obj;
+  };
 
 })("MathJax");
